513ddd204b7f21e473418029f76ede1892216426
Phillip Franco <pfranco266@gmail.com>
2024-04-03 11:54:54 +0200

    Added analytics to tthe project.jsx file to track which projects are being clicked

--- src/components/sections/Projects/Project.jsx
+++ src/components/sections/Projects/Project.jsx
@@ -3,33 +3,43 @@ import { TechItem, TechContainer, ProjectContainer, TextContainer, PictureContai
 import { Subtitle } from "../../../App.styled";
 import { Text } from "../Experience/Experience.styled";
 
-function Project ({projects}) {
+function Project({ projects }) {
+
+    const trackProjectClick = (projectName, projectUrl, event) => {
+        event.preventDefault(); // Prevent the default link behavior temporarily
+        window.gtag('event', 'select_content', {
+          content_type: 'project',
+          item_id: projectName
+        });
+        window.location.href = projectUrl; // Manually navigate to the project URL
+    };
+
     const listItems = projects.map((project) => (
         <ProjectContainer key={project.id}>
-            <a href={`${project.website}`} target="_blank" rel="noopener noreferrer">
-            <TextContainer>
-                <Subtitle>
-                    {project.name}
-                </Subtitle>
-                <Text>
-                    {project.description}
-                </Text>
-            </TextContainer>
+            <a href={`${project.website}`} onClick={(e) => trackProjectClick(project.name, project.website, e)} target="_blank" rel="noopener noreferrer">
+                <TextContainer>
+                    <Subtitle>
+                        {project.name}
+                    </Subtitle>
+                    <Text>
+                        {project.description}
+                    </Text>
+                </TextContainer>
 
-            <TechContainer>
+                <TechContainer>
                     {project.tech.map((element, index) => (
-                       <TechItem key={index}>{element}</TechItem> 
+                        <TechItem key={index}>{element}</TechItem>
                     ))}
-            </TechContainer>
+                </TechContainer>
             </a>
         </ProjectContainer>
-    ))
+    ));
 
     return (
         <>
             {listItems}
         </>
-    )
+    );
 }
 
 export default Project;
