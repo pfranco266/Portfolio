3aef573f0e0224b8c794a8bd7924787f01c017ef
Phillip Franco <pfranco266@gmail.com>
2024-04-08 08:38:40 +0200

    tag manager move to app.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -7,16 +7,20 @@ import About from './components/sections/About/About';
 import Experiences from './components/sections/Experience/Experiences';
 import Projects from './components/sections/Projects/Projects';
 import Footer from './components/sections/Footer/Footer';
+import TagManager from 'react-gtm-module';
 
-
+const tagManagerArgs = {
+  gtmId: 'GTM-M7JWTF5D',
+};
 
 function App() {
+  useEffect(() => {
+    TagManager.initialize(tagManagerArgs);
+  }, []); // The empty array ensures this effect runs once on mount
 
-  
   return (
-    
     <Main>
-      <GlobalStyle/>
+      <GlobalStyle />
       <NavbarContainer>
         <Navbar />
       </NavbarContainer>
@@ -27,7 +31,7 @@ function App() {
         <Footer />
       </ContentContainer>
     </Main>
-  )
+  );
 }
 
-export default App
+export default App;
--- src/main.jsx
+++ src/main.jsx
@@ -3,13 +3,7 @@ import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
 
-import TagManager from 'react-gtm-module';
 
-const tagManagerArgs = {
-  gtmId: 'GTM-M7JWTF5D',
-};
-
-TagManager.initialize(tagManagerArgs);
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
