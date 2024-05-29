7b9e69ed8f750e7a95f3440c9e0458c22c829a3f
Phillip Franco <pfranco266@gmail.com>
2024-05-29 08:17:06 +0700

    fix: spacing on resume

--- src/components/sections/Experience/Experiences.jsx
+++ src/components/sections/Experience/Experiences.jsx
@@ -1,5 +1,5 @@
 // External imports
-import React, {useContext, useState} from "react";
+import React, { useContext, useState } from "react";
 
 // Internal component imports
 import Experience from "./Experience";
@@ -15,44 +15,43 @@ import { ThemeContext } from "../../../Store/ThemeContext.jsx";
 
 
 
-function Experiences({experienceRef}) {
-  const {theme} = useContext(ThemeContext);
+function Experiences({ experienceRef }) {
+  const { theme } = useContext(ThemeContext);
 
-    const [clicked, setClicked] = useState(false);
+  const [clicked, setClicked] = useState(false);
 
-    function trackResumeDownload(resumeName) {
-      console.log(clicked);
-      setClicked((prev) => {
-        return prev === false ? true : false;
-      })
-        if (window.gtag) {
-          window.gtag('event', 'download', {
-            event_category: 'Resume',
-            event_label: resumeName,
-          });
-        }
-      }
-
-        return (
-            <Container ref={experienceRef} id="experiences">
-                <Title theme={theme}>EXPERIENCE</Title>
-                <Experience experiences={experiences} theme={theme} />
-                <ResumeContainer>
-                <a href="https://pfranco266.github.io/Portfolio/WebDev.pdf"
-                   download="Phil_Franco_WebDev.pdf"
-                   target="_blank" // Opens in a new tab
-                   onClick={() => trackResumeDownload('Web Dev Resume')}>
-                    <DownloadButton theme={theme}>Web Dev Resume <DownloadIcon /></DownloadButton>
-                </a>
-                <a href="https://pfranco266.github.io/Portfolio/Recruiter.pdf"
-                   download="Phil_Franco_Recruiter.pdf"
-                   target="_blank" // Opens in a new tab
-                   onClick={() => trackResumeDownload('Recruiter Resume')}>
-                    <DownloadButton theme={theme}>Recruiter Resume <DownloadIcon /></DownloadButton>
-                </a>
-            </ResumeContainer>
-            </Container>
-        );
+  function trackResumeDownload(resumeName) {
+    console.log(clicked);
+    setClicked((prev) => {
+      return prev === false ? true : false;
+    })
+    if (window.gtag) {
+      window.gtag('event', 'download', {
+        event_category: 'Resume',
+        event_label: resumeName,
+      });
     }
+  }
 
-    export default Experiences
+  return (
+    <Container ref={experienceRef} id="experiences">
+      <Title theme={theme}>EXPERIENCE</Title>
+      <Experience experiences={experiences} theme={theme} />
+      <ResumeContainer>
+        <a href="/webdev.pdf"
+          download="webdev.pdf"
+          onClick={() => trackResumeDownload('Web Dev Resume')}>
+          <DownloadButton theme={theme}>Web Dev Resume <DownloadIcon /></DownloadButton>
+        </a>
+        <a href="https://pfranco266.github.io/Portfolio/Recruiter.pdf"
+          download="Phil_Franco_Recruiter.pdf"
+          target="_blank" // Opens in a new tab
+          onClick={() => trackResumeDownload('Recruiter Resume')}>
+          <DownloadButton theme={theme}>Recruiter Resume <DownloadIcon /></DownloadButton>
+        </a>
+      </ResumeContainer>
+    </Container>
+  );
+}
+
+export default Experiences
